ae87ca0e02709b525d85c4942a836af44662a6bb
abdi syahputra harahap <44754630+abdiusu@users.noreply.github.com>
2020-03-22 13:04:30 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 	setTimeout(function() {
-		document.getElementById("myMessage").style.display = "none";
-	}, 100)
+		if (document.getElementById("myMessage")) {
+		document.getElementById("myMessage").remove();
+		};
+	}, 100);
 });
